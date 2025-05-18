04c1f598af9986ccd425698a60f8b4f854a83bab
Guénolé Joubioux <81358346+Guenoj@users.noreply.github.com>
2025-05-18 13:36:48 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# 1. Mise à jour et installation des paquets nécessaires
 apt-get update
-apt-get install -y ffmpeg libsm6 libxext6
+apt-get install -y \
+    ffmpeg \
+    libsm6 \
+    libxext6 \
+    libgl1-mesa-glx
+
+# 2. Remise des droits à l’utilisateur onyxia
 chown -R ${USERNAME}:${GROUPNAME} ${HOME}
