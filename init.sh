7de99324be7b416119daf7e18c07202bf2e71c11
Guénolé Joubioux <81358346+Guenoj@users.noreply.github.com>
2025-05-18 13:43:12 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-# 1. Mise à jour et installation des paquets nécessaires
+# 1. Mise à jour et paquets système (les plus indispensables)
 apt-get update
 apt-get install -y \
     ffmpeg \
     libsm6 \
-    libxext6 \
-    libgl1-mesa-glx
+    libxext6
 
-# 2. Remise des droits à l’utilisateur onyxia
+# 2. Instal’ Python “headless”
+python3 -m pip install --upgrade pip
+python3 -m pip install opencv-python-headless
+
+# 3. Rétablir droits
 chown -R ${USERNAME}:${GROUPNAME} ${HOME}
